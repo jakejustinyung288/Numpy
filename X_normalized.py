f7cc29a64c54f40e22c0010dbbb1aca69a7da41c
jakejustinyung288 <57263164+jakejustinyung288@users.noreply.github.com>
2019-11-30 14:04:11 +0800

    Add files via upload

---
+++ X_normalized.py
@@ -0,0 +1,10 @@
+import numpy as np 
+X=np.random.random((5,5))
+Y=(X-np.mean(X))/np.std(X)
+np.save('X_normalized.npy',Y)
+
+print('Random 5x5 array')
+print (X)
+print ('Normalized X')
+print (Y)
+
---
+++ div_by_3.py
@@ -0,0 +1,11 @@
+import numpy as np 
+x= np.arange(1,101,1).reshape(10,10)
+y=x*x
+z=y%3
+Div_by_3 =y[z == 0]
+np.save('div_by_3.npy',Div_by_3)
+
+print('Matrix Square 10x10 =')
+print (y)
+print('Numbers That are  Divisible by 3 in the Matrix Square 10x10:')
+print(Div_by_3)
